900c9843fe44be06ac1752972392ebbfdafa64d1
MihajloPetrovicDev <petrovicmihajlo789@gmail.com>
2024-12-18 17:44:32 +0100

    Create BaseHeadTags component and put it in home view head tag Create Header component and put it in home view body tag Add custom styles in app.css Add Bootstrap via CDN

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,20 @@
+<header id="main-header">
+    <div class="w-20p">
+        <a class="clean-link c-white-2 fs-2-5r" href="/">sklopi</a>
+    </div>
+    
+    <div class="d-flex align-items-center justify-content-center gap-5 w-60p mt-m-4px">
+        <a class="clean-link c-white-2 fs-1-25r fw-light hover-c-lgray" href="/">Početna Stranica</a>
+        <a class="clean-link c-white-2 fs-1-25r fw-light hover-c-lgray" href="/builder">Konfigurator</a>
+        <a class="clean-link c-white-2 fs-1-25r fw-light hover-c-lgray" href="/builds">Konfiguracije</a>
+        <a class="clean-link c-white-2 fs-1-25r fw-light hover-c-lgray" href="/discussions">Diskusije</a>
+    </div>
+
+    <div class="d-flex align-items-center ml-5 justify-content-end w-20p mt-m-4px">
+    @if(Auth::Check())
+        <a class="clean-link c-white-2 fs-1-25r hover-c-lgray" href="/my-account">Moj Nalog</a>
+    @else
+        <a class="clean-link c-white-2 fs-1-25r hover-c-lgray" href="/login">Prijavi Se</a>
+    @endif
+    </div>
+</header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <x-base-head-tags />
     <title>@lang('ui.home.header_title')</title>
-    <link rel="icon" href="{{ URL('/logos/small_logo.png') }}">
-    <link rel="stylesheet" href="{{ mix('resources/css/app.css'); }}">
 </head>
 
 <body>
+    <x-header />
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
