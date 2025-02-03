08ef6a23f7e531a6869e2445afedd29cc48452af
MihajloPetrovicDev <petrovicmihajlo789@gmail.com>
2025-02-03 02:51:03 +0100

    Change some configs

--- resources/views/components/base-head-tags.blade.php
+++ resources/views/components/base-head-tags.blade.php
@@ -3,7 +3,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" href="{{ URL('/images/ui/icon.jpg') }}">
-<link rel="stylesheet" href="{{ mix('resources/css/app.css'); }}">
+<link rel="stylesheet" href="{{ mix('css/app.css'); }}">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
